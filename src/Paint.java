c961667de9b88caffc9fa9442f9dcbe1e757af06
eitan613 <eitanadler613@gmail.com>
2019-02-04 05:14:01 -0500

    some final changes

--- src/Paint.java
+++ src/Paint.java
@@ -2,30 +2,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Paint extends JFrame {
+public class Paint {
+    private JFrame frame;
+    private JPanel canvas;
     private Painter painter;
-    private Color drawingColor = new Color(0,0,0);
+    private Color drawingColor;
 
     public static void main(String[] args) {
         new Paint();
     }
-    class CanvasPanel extends JPanel {
 
-        public CanvasPanel() {
-            setBackground(new Color(16, 248, 255));
-        }
-    }
     private Paint() {
-        setSize(600, 800);
-        JPanel canvas = new CanvasPanel();
-        add(BorderLayout.CENTER, canvas);
+        setFrame();
+        setPanel();
+        setButtons();
+        frame.setVisible(true);
+    }
 
-        JButton instructions = new JButton("Click and drag the mouse to draw the selected shape. Press here to " +
-                "choose colors.");
-        instructions.addActionListener(e -> {
+    private void setButtons() {
+        JPanel buttonsPanel = new JPanel();
+        frame.add(BorderLayout.EAST,buttonsPanel);
+
+        JButton drawingColorButton = new JButton("Press here to choose drawing colors.");
+        drawingColorButton.addActionListener(e -> {
             JColorChooser colorPanel = new JColorChooser();
             class okListener implements ActionListener{
-
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     drawingColor = colorPanel.getColor();
@@ -37,7 +38,20 @@ public class Paint extends JFrame {
             colorSetter.setVisible(true);
         });
 
-        add(BorderLayout.SOUTH, instructions);
+        JButton backgroundColorButton = new JButton("Press here to choose background color.");
+        backgroundColorButton.addActionListener(e -> {
+            JColorChooser colorPanel = new JColorChooser();
+            class okListener implements ActionListener{
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    canvas.setBackground(colorPanel.getColor());
+                }
+            }
+            colorPanel.setPreviewPanel(new JPanel());
+            JDialog colorSetter = JColorChooser.createDialog(null,"Pick a color",false,colorPanel,
+                    new okListener(),null);
+            colorSetter.setVisible(true);
+        });
 
         JButton lineButton = new JButton("Click to draw lines.");
         lineButton.addActionListener(e -> {
@@ -53,11 +67,26 @@ public class Paint extends JFrame {
         rectangleButton.addActionListener(e -> {
             painter = new RectanglePainter();
         });
+        JButton clear = new JButton("Clear the canvas");
+        clear.addActionListener(e ->{
+            canvas.revalidate();
+            canvas.repaint();
+        });
 
-        add(BorderLayout.EAST, lineButton);
-        add(BorderLayout.NORTH, ovalButton);
-        add(BorderLayout.WEST, rectangleButton);
+        buttonsPanel.setLayout(new BoxLayout(buttonsPanel,BoxLayout.Y_AXIS));
+        buttonsPanel.add(lineButton);
+        buttonsPanel.add(ovalButton);
+        buttonsPanel.add(rectangleButton);
+        buttonsPanel.add(drawingColorButton);
+        buttonsPanel.add(backgroundColorButton);
+        buttonsPanel.add(clear);
+    }
 
+    private void setPanel() {
+        canvas = new JPanel();
+        drawingColor = new Color(0,0,0);
+        canvas.setBackground(new Color(16, 248, 255));
+        frame.add(BorderLayout.CENTER, canvas);
         canvas.addMouseMotionListener(new MouseMotionListener() {
             @Override
             public void mouseDragged(MouseEvent mouseEvent) {
@@ -68,8 +97,11 @@ public class Paint extends JFrame {
             public void mouseMoved(MouseEvent mouseEvent) {
             }
         });
-        setSize(1000,700);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
+    }
+
+    private void setFrame() {
+        frame = new JFrame("Click and drag the mouse to draw the selected shape.");
+        frame.setSize(1000,700);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
